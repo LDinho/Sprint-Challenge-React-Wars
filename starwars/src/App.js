bf5820302bd68bef5c7dd1048ba5addbe3d499bd
LD <LDinho@users.noreply.github.com>
2019-03-09 15:04:21 -0500

    stretch-end- add next/prev button pagination functionality

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 import CharacterList from './components/CharacterList';
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      starwarsChars: []
+      starwarsChars: [],
+      next: '',
+      previous: '',
     };
   }
 
@@ -22,7 +24,12 @@ class App extends Component {
       })
       .then(data => {
         console.log('DATA:', data);
-        this.setState({ starwarsChars: data.results });
+        this.setState({
+          starwarsChars: data.results,
+          next: data.next,
+          previous: data.previous,
+
+        });
       })
       .catch(err => {
         throw new Error(err);
@@ -34,6 +41,16 @@ class App extends Component {
       <div className="App">
         <h1 className="Header">React Wars</h1>
         <CharacterList characters={this.state.starwarsChars} />
+
+        <button disabled={!this.state.previous} onClick={()=> {
+          this.getCharacters(this.state.previous);
+        }}>Previous
+        </button>
+
+        <button disabled={!this.state.next} onClick={()=> {
+          this.getCharacters(this.state.next);
+        }}>Next
+        </button>
 
       </div>
     );
